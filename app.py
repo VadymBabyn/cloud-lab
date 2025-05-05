8399f059beba50e119e1f6666c82adce35ad8281
YaTutZar6 <96494540+YaTutZar6@users.noreply.github.com>
2025-05-05 13:37:07 +0300

    Use PORT from environment for Render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Привіт з Flask!'
+    return 'Привіт з Flask на Render!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
